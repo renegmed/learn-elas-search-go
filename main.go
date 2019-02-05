3ae21c0db52a1e57f1334c37df1d90a05e6474b4
rene <renegmed@yahoo.com>
2019-02-05 12:07:19 -0500

    read_file: fixed, can read files and search item in indexed docs

--- main.go
+++ main.go
@@ -12,9 +12,6 @@ import (
 	"github.com/olivere/elastic"
 )
 
-const FIELD_TO_SEARCH = "topic"
-const PHRASE_TO_SEARCH = "line"
-
 type CsvLine struct {
 	topic  string
 	source string
@@ -32,9 +29,9 @@ func check(e error) {
 	}
 }
 
-func addToIndex(client *elastic.Client, id string, content Content) error {
+func addToIndex(client *elastic.Client, id string, topic string, content Content) error {
 	_, err := client.Index().
-		Index("line").
+		Index(topic).
 		Type("doc").
 		Id(id).
 		BodyJson(content).
@@ -55,59 +52,49 @@ func main() {
 	lines, err := csv.NewReader(file).ReadAll()
 	check(err)
 
-	csvFiles := make([]CsvLine, len(lines)-1)
+	csvFiles := make([]CsvLine, 0)
 
 	for _, line := range lines[1:] { // skip first line
-		//data := CsvLine{fmt.Sprintf("\"%v\"", line[0]), fmt.Sprintf("\"%v\"", line[1])}
 		data := new(CsvLine)
 		data.topic = line[0]
 		data.source = line[1]
 		csvFiles = append(csvFiles, *data)
 	}
 
-	// read each file and index their contents(lines)
-
-	content, err := ioutil.ReadFile("/Users/rene/learn/go-workspace/src/build-microservice-go/chapter3/main.go")
-	check(err)
-	//defer file.Close()
-	//fmt.Println(string(content))
-
-	// var fileLines []string
-
-	// scanner := bufio.NewScanner(content)
-
-	// scanner.Split(bufio.ScanLines)
-
-	// for scanner.Scan() {
-	// 	fileLines = append(fileLines, scanner.Text())
-	// }
-	// fmt.Println(fileLines)
-	// }
-
-	// Create a client
+	// Create a elasticsearch client
 	client, err := elastic.NewClient()
 	check(err)
+
+	// _, err = client.DeleteIndex("golang").Do(context.Background())
+	// check(err)
 
 	// Create an index
 	_, err = client.CreateIndex("golang").Do(context.Background())
 	check(err)
 
-	content2 := Content{Topic: "golang", Content: string(content), Source: "/Users/rene/learn/go-workspace/src/build-microservice-go/chapter3/main.go"}
-
 	counter := 1
-
-	addToIndex(client, strconv.Itoa(counter), content2)
+	// read each file and index their contents(lines)
+	for _, f := range csvFiles {
+		byteContents, err := ioutil.ReadFile(f.source)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			continue
+		}
+		content2 := Content{Topic: "golang", Content: string(byteContents), Source: f.source}
+		addToIndex(client, strconv.Itoa(counter), f.topic, content2)
+		counter++
+	}
 
 	// Search with a term query
 
-	termQuery := elastic.NewTermQuery("topic", "golang")
+	termQuery := elastic.NewTermQuery("content", "main")
 	searchResult, err := client.Search().
-		Index("golang").  // search in index "tweets"
-		Query(termQuery). // specify the query
-		//Sort("topic.keyword", true). // sort by "user" field, ascending
-		From(0).Size(10).        // take documents 0-9
-		Pretty(true).            // pretty print request and response JSON
-		Do(context.Background()) // execute
+		Index("golang").             // search in index "tweets"
+		Query(termQuery).            // specify the query
+		Sort("topic.keyword", true). // sort by "topic" field, ascending
+		From(0).Size(10).            // take documents 0-9
+		Pretty(true).                // pretty print request and response JSON
+		Do(context.Background())     // execute
 	check(err)
 
 	// searchResult is of type SearchResult and returns hits, suggestions,
@@ -127,23 +114,23 @@ func main() {
 	// TotalHits is another convenience function that works even when something goes wrong.
 	fmt.Printf("Found a total of %d hits\n", searchResult.TotalHits())
 
-	// // Here's how you iterate through results with full control over each step.
+	// Here's how you iterate through results with full control over each step.
 	// if searchResult.Hits.TotalHits > 0 {
-	// 	fmt.Printf("Found a total of %d tweets\n", searchResult.Hits.TotalHits)
+	// 	fmt.Printf("Found a total of %d hits\n", searchResult.Hits.TotalHits)
 
 	// 	// Iterate through results
 	// 	for _, hit := range searchResult.Hits.Hits {
 	// 		// hit.Index contains the name of the index
 
 	// 		// Deserialize hit.Source into a Tweet (could also be just a map[string]interface{}).
-	// 		var t Tweet
-	// 		err := json.Unmarshal(*hit.Source, &t)
+	// 		var c Content
+	// 		err := json.Unmarshal(*hit.Source, &c)
 	// 		if err != nil {
 	// 			// Deserialization failed
 	// 		}
 
 	// 		// Work with tweet
-	// 		fmt.Printf("Tweet by %s: %s\n", t.User, t.Message)
+	// 		fmt.Printf("Content Topic: %s   Source: %s\n", c.Topic, c.Source)
 	// 	}
 	// } else {
 	// 	// No hits

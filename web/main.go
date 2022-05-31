1ff08bc95ed3c9f31ba107e5b8b0fd27edad98b2
Rene Medalla <renemed@pop-os.localdomain>
2022-05-31 10:21:54 -0400

    added metrics prometheus and grafana

--- web/main.go
+++ web/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"embed"
 	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 //go:embed templates
@@ -11,6 +14,7 @@ var templates embed.FS
 func main() {
 	r := RegisterRoutes(&templates)
 	r.Static("/public", "./public")
-	log.Println("...server started :3000")
-	r.Run(":3000")
+	http.Handle("/metrics", promhttp.Handler())
+	log.Println("...server started :8080")
+	r.Run(":8080")
 }
